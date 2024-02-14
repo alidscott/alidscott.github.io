666784852ac5638190d26011cb1db125bbe120a8
Ali Scott <ali.diosabanscott@aa.com>
2024-02-13 23:00:20 -0600

    initial commit

--- src/app/AnimatedGif.tsx
+++ src/app/AnimatedGif.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
 export const AnimatedGif = ({ src, alt }: { src: string, alt: string }) => {
-    return (<img src={src} alt={alt} height={200} width={200} />);
+    return (<img src={src} alt={alt} height={300} width={300} />);
     
 }
